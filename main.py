9783c96e626530b068f226a3147643a4b3063c5f
Pradyuman <pradyumaann@gmail.com>
2024-10-21 00:37:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,20 +4,85 @@ import matplotlib.pyplot as plt
 import seaborn as sns 
 
 import xgboost as xgb
+from sklearn.metrics import mean_squared_error
+
 color_pal = sns.color_palette()
 plt.style.use('fivethirtyeight')
 
 df = pd.read_csv('PJME_hourly.csv')
 df = df.set_index('Datetime')
+df.index = pd.to_datetime(df.index)
 
-#df.plot(style='.', figsize=(15, 5), color=color_pal[0], title='PJME Enery Use in MW')
-pd.to_datetime(df.index)
-
+df.plot(style='.',
+        figsize=(15, 5),
+        color=color_pal[0], 
+        title='PJME Enery Use in MW')
+ 
 #split the data into 2 parts from 2008 to 2015, for training the model; and from 2015 to 2018 for testin the model
-train = df.loc[df.index < '2015-01-01']
-test = df.loc[df.index >= '2015-01-01']
+train = df.loc[df.index < '01-01-2015']
+test = df.loc[df.index >= '01-01-2015']
 
+fig, ax = plt.subplots(figsize=(15, 5))
+train.plot(ax=ax, label='Training Set')
+test.plot(ax=ax, label='Test Set')
+ax.axvline('2015-01-01', color='black', ls='--')
+ax.legend(['Training Set', 'Test Set'])
+plt.show()
 
-#one single week of energy consumption
-df.loc[(df.index > '2010-01-01') & (df.index < '2010-01-08')].plot(figsize=(15, 5), title='Week of Data')
+#creating Features from the Time Series dataset
+def create_features(df):
+    df['hour'] = df.index.hour
+    df['dayofweek'] = df.index.day_of_week
+    df['quarter'] = df.index.quarter
+    df['month'] = df.index.month
+    df['year'] = df.index.year
+    df['dayofyear'] = df.index.dayofyear
+    return df
+
+df = create_features(df)
+
+#Visualize our features/target relationship 
+fig, ax = plt.subplots(figsize=(10, 8))
+sns.boxplot(data=df, x='hour', y='PJME_MW', palette='Reds') 
+ax.set_title('MW by hour')
+
+fig, ax = plt.subplots(figsize=(10, 8))
+sns.boxplot(data=df, x='month', y='PJME_MW', palette='Blues') 
+ax.set_title('MW by month')
+plt.show()
+
+#Creating the machine learning model
+train = create_features(train)
+test = create_features(test)
+
+FEATURES = ['hour', 'dayofweek', 'quarter', 'month', 'year', 'dayofyear']
+TARGET = 'PJME_MW'
+
+X_train = train[FEATURES]
+Y_train = train[TARGET]
+
+X_test = test[FEATURES]
+Y_test = test[TARGET]
+
+reg = xgb.XGBRegressor(n_estimators=1000, early_stopping_rounds=50,
+                       learning_rate=0.01) 
+reg.fit(X_train, Y_train,
+        eval_set=[(X_train, Y_train), (X_test, Y_test)],
+        verbose=100)
+
+#Feature importances
+fi = pd.DataFrame(data=reg.feature_importances_,
+                  index=reg.feature_names_in_,
+                  columns=['Coefficients'])
+
+fi.sort_values('Coefficients').plot(kind='bar', title='Feature Coefficients')
+
+#Forecast on the testing dataset
+test['prediction'] = reg.predict(X_test)
+df = df.merge(test[['prediction']], how='left', left_index=True, right_index=True)
+
+ax = df[['PJME_MW']].plot(figsize=(15, 5))
+df['prediction'].plot(ax=ax, style='.')
+plt.legend(['Truth data', 'Predicitons'])
+ax.set_title('Raw Data and Predictions')
 plt.show()
