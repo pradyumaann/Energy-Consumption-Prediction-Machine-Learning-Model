5f56af568ffaa68dfb85f8f2606c3f22de9794ec
Pradyuman <pradyumaann@gmail.com>
2024-10-20 05:01:05 +0100

    Initial commit

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns 
+
+import xgboost as xgb
+color_pal = sns.color_palette()
+plt.style.use('fivethirtyeight')
+
+df = pd.read_csv('PJME_hourly.csv')
+df = df.set_index('Datetime')
+
+df.plot(style='.', figsize=(15, 5), color=color_pal[0], title='PJME Enery Use in MW')
+df
