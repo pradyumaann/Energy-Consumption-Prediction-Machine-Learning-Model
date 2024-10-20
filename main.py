8e69244dc2e8d636cc430da6b4bbb8280590a7cc
Pradyuman <pradyumaann@gmail.com>
2024-10-20 23:38:41 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,5 +10,14 @@ plt.style.use('fivethirtyeight')
 df = pd.read_csv('PJME_hourly.csv')
 df = df.set_index('Datetime')
 
-df.plot(style='.', figsize=(15, 5), color=color_pal[0], title='PJME Enery Use in MW')
-df
+#df.plot(style='.', figsize=(15, 5), color=color_pal[0], title='PJME Enery Use in MW')
+pd.to_datetime(df.index)
+
+#split the data into 2 parts from 2008 to 2015, for training the model; and from 2015 to 2018 for testin the model
+train = df.loc[df.index < '2015-01-01']
+test = df.loc[df.index >= '2015-01-01']
+
+
+#one single week of energy consumption
+df.loc[(df.index > '2010-01-01') & (df.index < '2010-01-08')].plot(figsize=(15, 5), title='Week of Data')
+plt.show()
